078d26fc400ed686edb0014109b66ab00bc7b6a0
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2021-01-23 00:32:32 +0000

    Client -- implemented home controller and started work on sign in controller

---
+++ Client/bisMateClient/bisMateClient/HTTP/RestClient.swift
@@ -0,0 +1,8 @@
+//
+//  RestClient.swift
+//  bisMateClient
+//
+//  Created by Razvan-Antonio Berbece on 22/01/2021.
+//
+
+import Foundation
---
+++ Client/bisMateClient/bisMateClient/UILogic/FadeExtension/UIView.swift
@@ -0,0 +1,8 @@
+//
+//  UIView.swift
+//  bisMateClient
+//
+//  Created by Razvan-Antonio Berbece on 22/01/2021.
+//
+
+import Foundation
