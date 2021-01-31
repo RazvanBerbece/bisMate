dcb4d6d0a3d0f1f460c59912d0ef34761af09b7c
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2021-01-31 19:33:33 +0000

    Integrating sockets logic into messaging app

--- Client/bisMateClient/bisMateClient/Controllers/MessageDetailViewController.swift
+++ Client/bisMateClient/bisMateClient/Controllers/MessageDetailViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class MessageDetailViewController: UIViewController {
     
+    // UI Components
+    @IBOutlet weak var fieldMessageInput: UITextField!
+    
     // Message Logic from previous view
     var MessageWithUserID : String?
     
+    // Sockets
+    var socketClient : SocketClient?
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // print(MessageWithUserID)
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction private func sendMessageAction() {
+        // TODO
+        let input = self.fieldMessageInput.text
+        print(input)
     }
     
 }
