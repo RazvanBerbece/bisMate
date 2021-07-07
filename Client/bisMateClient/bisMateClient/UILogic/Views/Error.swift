1a929e1c63efcf78375e5e1140e3ce66094afcb2
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2021-07-07 22:47:02 +0100

    Presync commit

--- Client/bisMateClient/bisMateClient/UILogic/Views/Error.swift
+++ Client/bisMateClient/bisMateClient/UILogic/Views/Error.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+/**
+ Takes in an error string and returns an UIView for that error
+ */
+func getErrorView(error: String) -> UIView {
+    
+    let screenSize = UIScreen.main.bounds;
+    
+    // Initialize view that will be swiped
+    // 90% of width (empty 5% left 5% right), 73.33% of height
+    let view = UIView(frame: CGRect(origin: CGPoint(x: (5/100) * screenSize.width, y: 125.0), size: CGSize(width: (90/100) * screenSize.width, height: (73.33/100) * screenSize.height)))
+    
+    // Subviews (profile data) to be added to swipeable view
+    let errorMessageLabel = UILabel(frame: CGRect(x: view.frame.origin.x, y: view.frame.origin.y, width: view.frame.width * 0.75, height: view.frame.height * 0.2))
+    errorMessageLabel.textColor = .label
+    errorMessageLabel.textAlignment = .center
+    errorMessageLabel.center = CGPoint(x: view.frame.width / 2, y: view.frame.height * 0.5)
+    
+    // Subivew data logic
+    errorMessageLabel.text = error
+    
+    // Add subviews
+    view.addSubview(errorMessageLabel)
+    
+    // Configration
+    view.translatesAutoresizingMaskIntoConstraints = false
+    
+    return view
+    
+}
