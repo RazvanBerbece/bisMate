ca0a0e0240682083b3485ac05b5d029da0468b42
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2021-08-16 00:44:38 +0100

    Extended unit testing

--- Server/src/firebase/fbadmin_test.go
+++ Server/src/firebase/fbadmin_test.go
@@ -1,6 +1,7 @@
 package fbadmin_test
 
 import (
+	"container/list"
 	"testing"
 
 	fbadmin "bismateServer/firebase"
@@ -58,6 +59,101 @@ func TestChangeDisplayName(t *testing.T) {
 		}
 	default:
 		t.Error("GetUserProfile() status is unrecognised")
+	}
+
+}
+
+func TestGetUIDFromLocation(t *testing.T) {
+	// Configure vars
+	App := fbadmin.FirebaseApp{}
+	App.InitFirebase()
+
+	testingUID := "hmHIk2gjMCScZimPXzziVDzf2Pz1"
+	testingLocation := "TestGetLocation"
+
+	// Add user to location first
+	status := -1
+	list := list.List{}
+	App.GetUIDFromLocation(&status, testingLocation, &list)
+	if status == -1 {
+		t.Error("GetUIDFromLocation() should be called & finish executing")
+	} else if status == 0 {
+		t.Error("GetUIDFromLocation() should save the UID to the location sucessfully")
+	} else {
+		// Iterate the list
+		// if the testinf UID is found in the list, success
+		success := 0
+		for e := list.Front(); e != nil; e = e.Next() {
+			uid := e.Value.(string)
+			if uid == testingUID {
+				success = 1
+				break
+			}
+		}
+		if success == 0 {
+			t.Error("GetUIDFromLocation() the UID couldn't be get from the location")
+		}
+	}
+
+}
+
+func TestSaveUIDToLocation(t *testing.T) {
+
+	// Configure vars
+	App := fbadmin.FirebaseApp{}
+	App.InitFirebase()
+
+	testingUID := "hmHIk2gjMCScZimPXzziVDzf2Pz1"
+	testingLocation := "TestLocation"
+
+	// Add user to location first
+	status := -1
+	App.SaveUIDToLocation(&status, testingUID, testingLocation)
+
+	if status == -1 {
+		t.Error("SaveUIDToLocation() should be called & finish executing")
+	} else if status == 0 {
+		t.Error("SaveUIDToLocation() should save the UID to the location sucessfully")
+	} else {
+		status := -1
+		list := list.List{}
+		App.GetUIDFromLocation(&status, testingLocation, &list)
+		// Iterate the list
+		// if the testinf UID is found in the list, success
+		success := 0
+		for e := list.Front(); e != nil; e = e.Next() {
+			uid := e.Value.(string)
+			if uid == testingUID {
+				success = 1
+				break
+			}
+		}
+		if success == 0 {
+			t.Error("SaveUIDToLocation() the UID couldn't be found in the location")
+		}
+		// SUCCESS, DO NOTHING, REMOVE TEST PROCESSES NEXT AND HANDLES CLEANUP TOO
+	}
+
+}
+
+func TestRemoveUIDFromLocation(t *testing.T) {
+
+	// THIS WILL PASS ONLY IF TestSaveUIDToLocation() IS CALLED BEFORE THIS FUNCTION
+	// OR IF SaveUIDToLocation() is called here
+
+	// Configure vars
+	App := fbadmin.FirebaseApp{}
+	App.InitFirebase()
+
+	testingUID := "hmHIk2gjMCScZimPXzziVDzf2Pz1"
+	testingLocation := "TestLocation"
+
+	// Add user to location first
+	_, fail := App.RemoveUIDFromLocation(testingUID, testingLocation)
+	if fail != "" {
+		t.Errorf("RemoveUIDFromLocation() failed; %s", fail)
+	} else {
+		// SUCCESS, DO NOTHING
 	}
 
 }
